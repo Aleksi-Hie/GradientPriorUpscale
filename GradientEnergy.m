f9665bd22c0bede6167b3a5c77f1263643b0648d
Aleksi-Hie <63103552+Aleksi-Hie@users.noreply.github.com>
2024-04-21 19:09:41 +0300

    Update GradientEnergy.m

--- GradientEnergy.m
+++ GradientEnergy.m
@@ -1,9 +1,7 @@
 function gradient_E = GradientEnergy(Ih, Il, G, beta)
-    % Compute the smoothed and downsampled version of HR image Ih
     Ih_smooth_down = imfilter(Ih, G, 'same');
     Ih_smooth_down = imresize(Ih_smooth_down, size(Il), 'bicubic');
     
-    % Compute the difference between the LR image Il and the smoothed and downsampled version of HR image Ih
     diff = Ih_smooth_down - Il;
     
     % Upsample the difference and convolve with G
@@ -13,8 +11,6 @@ function gradient_E = GradientEnergy(Ih, Il, G, beta)
     % Compute the Laplacian of Ih and ITh
     lap_Ih = del2(Ih);
     lap_ITh = imresize(del2(HRGradientTransform(Ih, Il)), size(Ih), 'bicubic');
-    
-    % Compute the difference between the Laplacians
     lap_diff = lap_Ih - lap_ITh;
     
     % Compute the gradient of the energy function
